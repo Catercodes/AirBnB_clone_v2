242ba075b05a52eaa7a670e0abc043bbdc331dce
Catercodes <chrisadamsadewuni@gmail.com>
2024-04-17 22:49:23 +0100

    start flask web application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,9 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_world():
+    """Script that start flask web application"""
+    return "Hello HBNB!"
