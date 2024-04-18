aa78b01515aa3090b027d8dc05aa65002a8c69ba
Catercodes <chrisadamsadewuni@gmail.com>
2024-04-18 15:51:57 +0100

    Python is cool!

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""My web application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """home route function"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text(text):
+    new_text = text.replace('_', ' ')
+    return f"C {new_text}"
+
+
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    new_text = text.replace('_', ' ')
+    return f"python {new_text}"
+
+
+@app.route('/python', strict_slashes=False)
+def python():
+    return "python is cool"
+
+
+if __name__ == "__main__":
+    app.run()
