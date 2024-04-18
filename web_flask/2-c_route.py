dcce1b654df67601ccaa98a8f9d6dd8a7a7080d7
Catercodes <chrisadamsadewuni@gmail.com>
2024-04-18 05:45:42 +0100

    C is fun!

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""My web application"""
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """home route function"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text(text):
+    new_text = text.replace('_', ' ')
+    return f"C {new_text}"
+
+
+if __name__ == "__main__":
+    app.run()
